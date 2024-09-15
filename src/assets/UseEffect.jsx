c69a7f775ce20a25ee83db8f3325b163e366f3d5
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-15 21:13:20 +0500

    Initial commit

---
+++ src/assets/UseEffect.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+function UseEffect() {
+    const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const getProducts = () => {
+        fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((res) => {
+            console.log("res->", res );
+            
+            setProducts(res);
+        });
+    };
+
+    const searched = products?.filter(
+        (data) => data.title.toLowerCase().indexOf(search) !== -1
+    );
+    retrun (
+        <div>
+            <h1>Learning UseEffect</h1>
+            <input 
+            placeholder="Search"
+            type="search"
+            className="border w-3/4 p-2" 
+            onChange={(e) => setSearch(e.target.value)}/>
+            {searched.map((data) => (
+                <h1 className="text-left p-2 my-1 bg-purple-200" key={data.id}>{data.id + ") "} {data.title} </h1>
+            ))}
+        </div>
+    );
+}
+
+export default UseEffect;
