cb34113b5456bb4932969ed85295e94be473828d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-16 13:01:57 +0500

    add to commit upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import UseEffect from './assets/UseEffect'
-import { Box, Typography, Button, TextField} from '@mui/material'
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import UseEffect from "./assets/UseEffect";
+import { Box, Typography, Button, TextField } from "@mui/material";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 function App() {
- 
-
   return (
-    <div className='container mx-auto'>
-      <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} alignItems={'center'}
-       bgcolor={'orange'} padding={1} color={'white'} marginTop={1} marginBottom={1} borderRadius={3}>
-<Typography variant='h5'fontWeight={'medium'} sx={{textDecoration:'underline'}}>Image Gallery</Typography>
-<Button variant='outlined'
-color='white'
-endIcon={<AccountCircleOutlinedIcon/>}
->Login </Button></Box>
-<Box display={'flex'} justifyContent={'space-between'}>
-<TextField id="outlined-basic" label="Search Your Images" variant="outlined" />
-
-</Box>
+    <div className="container mx-auto">
+      <Box
+        display={"flex"}
+        flexDirection={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        bgcolor={"orange"}
+        padding={1}
+        color={"white"}
+        marginTop={1}
+        marginBottom={1}
+        borderRadius={3}
+      >
+        <Typography
+          variant="h5"
+          fontWeight={"medium"}
+          sx={{ textDecoration: "underline" }}
+        >
+          Image Gallery
+        </Typography>
+        <Button
+          variant="outlined"
+          color="white"
+          endIcon={<AccountCircleOutlinedIcon />}
+        >
+          Login{" "}
+        </Button>
+      </Box>
+      <Box display={"flex"} gap={2} justifyContent={"space-between"}>
+        <TextField
+          id="outlined-basic"
+          label="Search Your Images"
+          fullWidth
+          variant="outlined"
+        />
+        <Button
+          variant="outlined"
+          color="primary"
+          endIcon={<AccountCircleOutlinedIcon />}
+        >
+          Login{" "}
+        </Button>
+      </Box>
     </div>
-  
-  )
+  );
 }
 
-export default App
+export default App;
