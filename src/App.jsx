cdbdc8485f231a1221e59097e298fd61075f1fb5
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-16 11:52:51 +0500

    add to commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
 import UseEffect from './assets/UseEffect'
+import { Box, Typography} from '@mui/material'
 
 function App() {
  
 
   return (
     <div className='container mx-auto'>
-<h1>Learning Material UI</h1>
+      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}
+       bgcolor={'orange'} padding={1} color={'white'} marginTop={1} marginBottom={1} borderRadius={3}>
+<Typography variant='h4'>Learning Material UI</Typography></Box>
     </div>
   
   )
